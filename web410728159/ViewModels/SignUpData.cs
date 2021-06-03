bf730ac648dae72ba08729848859ab49b8daf40b
陳子妤 <scorpiowua@gmail.com>
2021-06-03 21:03:09 +0800

    Model - signup

---
+++ web410728159/ViewModels/SignUpData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web410728159.ViewModels
+{
+    public class SignUpData
+    {
+        public String Name { get; set; }
+        public String Account { get; set; }
+        public String Password { get; set; }
+        public String NameMessage { get; set; }
+        public String AccountMessage { get; set; }
+        public String PasswordMessage { get; set; }
+        public String Message { get; set; }
+
+    }
+}
